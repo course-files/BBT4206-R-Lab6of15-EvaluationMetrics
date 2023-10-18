6956fcc8185a03f529520c34e2aac8b86faa1f7a
Course Files <137632706+course-files@users.noreply.github.com>
2023-10-18 05:59:25 +0300

    Corrected lab title

--- Lab6-EvaluationMetrics.R
+++ Lab6-EvaluationMetrics.R
@@ -1,5 +1,5 @@
 # *****************************************************************************
-# Lab 5: Evaluation Metrics ----
+# Lab 6: Evaluation Metrics ----
 #
 # Course Code: BBT4206
 # Course Name: Business Intelligence II
